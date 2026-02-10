26d61ae030307df51d5dfa10685c7bb29df23a30
sotti <sottilitommaso@gmail.com>
2026-02-10 17:06:45 +0100

    Implementazione classe Date

--- Date.cpp
+++ Date.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Date.h"
+
+Date::Date(int day, int month, int year) {
+    this->day = day;
+    this->month = month;
+    this->year = year;
+}
+
+int Date::getDay() const {
+    return this->day;
+}
+
+int Date::getMonth() const {
+    return this->month;
+}
+
+int Date::getYear() const {
+    return this->year;
+}
+
+void Date::setDay(int d) {
+    this->day = d;
+}
+
+void Date::setMonth(int m) {
+    this->month = m;
+}
+
+void Date::setYear(int y) {
+    this->year = y;
+}
