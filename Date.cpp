f0f7122e0e6804eb0ab5e954f13b54fe21492419
sotti <sottilitommaso@gmail.com>
2026-02-10 17:00:06 +0100

    dichiarazione classe Date

---
+++ Date.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sotti on 10/02/2026.
+//
+
+#include "Date.h"
---
+++ Date.h
@@ -0,0 +1,29 @@
+//
+// Created by sotti on 10/02/2026.
+//
+
+#ifndef TODOLIST_DATE_H
+#define TODOLIST_DATE_H
+
+
+class Date {
+
+private:
+    int day;
+    int month;
+    int year;
+
+public:
+    Date(int day, int month, int year);
+
+    int getDay() const;
+    int getMonth() const;
+    int getYear() const;
+
+    void setDay(int d);
+    void setMonth(int m);
+    void setYear(int y);
+};
+
+
+#endif //TODOLIST_DATE_H
