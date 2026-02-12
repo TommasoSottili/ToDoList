dd2b6624908a1e38c366673f84c7ff80cadd7ab6
sotti <sottilitommaso@gmail.com>
2026-02-12 16:37:25 +0100

    dichiarazione classe Todo con i metodi iniziali

---
+++ ToDo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sotti on 12/02/2026.
+//
+
+#include "ToDo.h"
---
+++ ToDo.h
@@ -0,0 +1,25 @@
+//
+// Created by sotti on 12/02/2026.
+//
+
+#ifndef TODOLIST_TODO_H
+#define TODOLIST_TODO_H
+#include <string>
+#include "Date.h"
+
+class ToDo {
+private:
+    Date date;
+    bool completed;
+    std::string description;
+public:
+    ToDo(const Date &date, const std::string& description);
+
+    void setDescription(const std::string& description);
+    void setDate(const Date &date);
+    void setCompleted(bool completed);
+    std::string getDescription() const;
+};
+
+
+#endif //TODOLIST_TODO_H
