c4a9d7beb43e4e34bdb28702cc5f660355084fc8
sotti <sottilitommaso@gmail.com>
2026-02-13 15:54:20 +0100

    implementazione test per metodi principali della classe ToDoList

--- Test/ToDoListTest.cpp
+++ Test/ToDoListTest.cpp
@@ -18,3 +18,38 @@ protected:
     }
 };
 
+TEST_F(ToDoListTest, CountsCorrect) {
+    EXPECT_EQ(list.totalToDoCount(),3);
+    list.ToDoCompleted("Compito A");
+    EXPECT_EQ(list.uncompletedToDoCount(),2);
+    list.ToDoCompleted("Compito A");
+}
+
+TEST_F(ToDoListTest, ListRetrieval) {
+    EXPECT_EQ(list.getToDoList().size(),3);
+    list.ToDoCompleted("Compito A");
+    std::list<ToDo> uncompletedToDoList = list.getUncompletedTodo();
+    EXPECT_EQ(uncompletedToDoList.size(),2);
+    EXPECT_EQ(uncompletedToDoList.front().getDescription(), "Compito B");
+}
+
+TEST_F(ToDoListTest, RemoveAllTodos) {
+    list.removeAllToDo();
+    EXPECT_EQ(list.totalToDoCount(),0);
+    EXPECT_EQ(list.uncompletedToDoCount(),0);
+    EXPECT_TRUE(list.getToDoList().empty());
+}
+
+TEST_F(ToDoListTest, ChangeCompleted) {
+    list.ToDoCompleted("Compito A");
+    list.ToDoCompleted("Compito B");
+    EXPECT_EQ(list.uncompletedToDoCount(),1);
+    bool fad = false;
+    for (const auto& item : list.getToDoList()) {
+        if (item.getDescription() == "Compito A" &&  item.isCompleted()) {
+            fad = true;
+        }
+    }
+    EXPECT_TRUE(fad);
+}
+
